bd332051cf6c353741a4757013ea501221a69632
nouha18 <nouha.mbarek7@gmail.com>
2022-08-20 15:14:41 +0200

    fix: build configuration db

--- src/setup/dbconfig.js
+++ src/setup/dbconfig.js
@@ -1,6 +1,15 @@
 var mongoose =require( 'mongoose');
+var dbconfig = require('../../config/default');
+const config = require('config');
+//...
+const dbConfig = config.get('User.dbConfig.dbName');
+
  var mongoDB =process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/myDB' ;
- mongoose.connect(mongoDB,{useNewUrlParser:true,useUnifiedTopology:true});
+ mongoose.connect(dbConfig,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+     console.log('database connected')
+ }).catch((err)=>{
+     console.log('Error Credentials Db',err)
+ });
  mongoose.Promise = global.Promise;
  //var DB = mongoose.createConnection('mongodb://localhost:27017/mydb');
 
